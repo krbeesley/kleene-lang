97080bb0af77b73de0ae7699d02f6a2eaf37b91f
krbeesley <krbeesley@gmail.com>
2012-11-04 23:21:46 -0700

    v0.9.3.1 commit of master branch

--- kleene/src/main/java/org/kleene-lang/KleeneGuiConstants.java
+++ kleene/src/main/java/org/kleene-lang/KleeneGuiConstants.java
@@ -22,7 +22,7 @@
 //	Constants used in the Kleene GUI
 
 public interface KleeneGuiConstants {
-	public static final String VERSION_NUMBER = "0.9.3.0" ;
+	public static final String VERSION_NUMBER = "0.9.3.1" ;
 
 	public static final String COPYRIGHT_YEAR_RANGE = "2006-2012" ;
 }
